6dca239cf92342ba403f9f89da9409e18486172c
Erik Sohns <eriksohns@123mail.org>
2015-07-09 03:09:39 +0100

    minor changes

--- prj/solstudio/libACEStream/nbproject/private/Makefile-variables.mk
+++ prj/solstudio/libACEStream/nbproject/private/Makefile-variables.mk
@@ -4,16 +4,16 @@
 # NOCDDL
 #
 # Debug configuration
-CND_SYSINCLUDES_C_Debug=/opt/sunstudio12.1/prod/include/cc,/usr/include
-CND_SYSINCLUDES_CPP_Debug=/opt/sunstudio12.1/prod/include/CC/Cstd,/opt/sunstudio12.1/prod/include/CC/std,/opt/sunstudio12.1/prod/include/CC,/opt/sunstudio12.1/prod/include/CC/rw7,/opt/sunstudio12.1/prod/include/cc,/usr/include
-CND_SYSINCLUDES_CPP_Debug=/opt/sunstudio12.1/prod/include/CC/Cstd,/opt/sunstudio12.1/prod/include/CC/std,/opt/sunstudio12.1/prod/include/CC,/opt/sunstudio12.1/prod/include/CC/rw7,/opt/sunstudio12.1/prod/include/cc,/usr/include
-PROC_PRECOMPILER_Debug=/opt/sunstudio12.1/OIC/sdk/proc
-OIC_LIBRARY_PATH_Debug=/opt/sunstudio12.1/OIC
-OIC_INCLUDE_PATH_Debug=/opt/sunstudio12.1/OIC/sdk/include
+CND_SYSINCLUDES_C_Debug=/opt/solarisstudio12.4/lib/compilers/include/cc,/usr/include
+CND_SYSINCLUDES_CPP_Debug=/opt/solarisstudio12.4/lib/compilers/include/CC/Cstd,/opt/solarisstudio12.4/lib/compilers/include/CC,/opt/solarisstudio12.4/lib/compilers/include/cc,/usr/include
+CND_SYSINCLUDES_CPP_Debug=/opt/solarisstudio12.4/lib/compilers/include/CC/Cstd,/opt/solarisstudio12.4/lib/compilers/include/CC,/opt/solarisstudio12.4/lib/compilers/include/cc,/usr/include
+PROC_PRECOMPILER_Debug=/opt/solarisstudio12.4/OIC/sdk/proc
+OIC_LIBRARY_PATH_Debug=/opt/solarisstudio12.4/OIC
+OIC_INCLUDE_PATH_Debug=/opt/solarisstudio12.4/OIC/sdk/include
 # Release configuration
-CND_SYSINCLUDES_C_Release=/opt/sunstudio12.1/prod/include/cc,/usr/include
-CND_SYSINCLUDES_CPP_Release=/opt/sunstudio12.1/prod/include/CC/Cstd,/opt/sunstudio12.1/prod/include/CC/std,/opt/sunstudio12.1/prod/include/CC,/opt/sunstudio12.1/prod/include/CC/rw7,/opt/sunstudio12.1/prod/include/cc,/usr/include
-CND_SYSINCLUDES_CPP_Release=/opt/sunstudio12.1/prod/include/CC/Cstd,/opt/sunstudio12.1/prod/include/CC/std,/opt/sunstudio12.1/prod/include/CC,/opt/sunstudio12.1/prod/include/CC/rw7,/opt/sunstudio12.1/prod/include/cc,/usr/include
-PROC_PRECOMPILER_Release=/opt/sunstudio12.1/OIC/sdk/proc
-OIC_LIBRARY_PATH_Release=/opt/sunstudio12.1/OIC
-OIC_INCLUDE_PATH_Release=/opt/sunstudio12.1/OIC/sdk/include
+CND_SYSINCLUDES_C_Release=/opt/solarisstudio12.4/lib/compilers/include/cc,/usr/include
+CND_SYSINCLUDES_CPP_Release=/opt/solarisstudio12.4/lib/compilers/include/CC/Cstd,/opt/solarisstudio12.4/lib/compilers/include/CC,/opt/solarisstudio12.4/lib/compilers/include/cc,/usr/include
+CND_SYSINCLUDES_CPP_Release=/opt/solarisstudio12.4/lib/compilers/include/CC/Cstd,/opt/solarisstudio12.4/lib/compilers/include/CC,/opt/solarisstudio12.4/lib/compilers/include/cc,/usr/include
+PROC_PRECOMPILER_Release=/opt/solarisstudio12.4/OIC/sdk/proc
+OIC_LIBRARY_PATH_Release=/opt/solarisstudio12.4/OIC
+OIC_INCLUDE_PATH_Release=/opt/solarisstudio12.4/OIC/sdk/include
